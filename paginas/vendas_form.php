ad117696a35fa1512f007b3e1fd7fd3b5a927b52
Álvaro Felipe <alvarofelipems@gmail.com>
2013-09-02 09:33:57 -0700

    Update

--- paginas/vendas_form.php
+++ paginas/vendas_form.php
@@ -1,4 +1,55 @@
 <style>
+	body {background-color:blue;}
+<style>
+	
+</style>
+<div style="width:400px; margin:0 auto; border:1px solid black;">
+    <form id="vendas">
+        <label for="dataPedido">
+            <ul>
+            	<li>Data do pedido:</li> <li><input type="date" name="dataPedido" id="dataPedido" value="<?php echo date('Y-m-d'); ?>"/></li>
+            </ul>
+            <div style="clear:both"></div>
+        </label>
+        
+        <label for="dataCompra">
+            <ul>
+            	<li>Data da compra:</li> <li><input type="date" name="dataCompra" id="dataCompra"  value="<?php echo date('Y-m-d'); ?>"/></li>
+        	</ul>
+            <div style="clear:both"></div>
+        </label>
+        
+        <label for="cliente">
+        	<ul>
+        		<li>Cliente:</li> <li><input type="text" name="cliente" id="cliente"/></li>
+         	</ul>
+            <div style="clear:both"></div>
+        </label>
+        
+        <label for="email">
+        	<ul>
+        		<li>E-Mail:</li> <li><input type="email" name="email" id="email"/></li>
+         	</ul>
+            <div style="clear:both"></div>
+        </label>
+        
+        <label for="pedidoCOD">
+        	<ul>
+        		<li>pedidoCOD:</li> <li><input type="text" name="pedidoCOD" id="pedidoCOD"/></li>
+         	</ul>
+            <div style="clear:both"></div>
+        </label>
+        
+         <label for="rastreioCOD">
+        	<ul>
+        		<li>rastreioCOD:</li> <li><input type="text" name="rastreioCOD" id="rastreioCOD"/></li>
+         	</ul>
+            <div style="clear:both"></div>
+        </label>
+    </form>
+    <div style="clear:both"></div>
+=======
+<style>
 	body {background-color:red;}
 </style>
 <div style="width:400px; margin:0 auto; border:1px solid black;">
@@ -46,4 +97,5 @@
         </label>
     </form>
     <div style="clear:both"></div>
+>>>>>>> .r7
 </div>
